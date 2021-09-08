1a1432356b14da6257039d7f3f13b812961faf78
Luiz Henrique <43296232+henriquesaydt@users.noreply.github.com>
2021-09-08 14:02:24 -0300

    Rota desaparecido concluida

--- nuxt-Finder/api/routes/desaparecido.js
+++ nuxt-Finder/api/routes/desaparecido.js
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 //ID do recurso no banco
-const recursoId = 5;
+const recursoId = 6;
 
 async function authorization (req, res, next) {
   var permissaoId = 0;
@@ -125,7 +125,7 @@ router.post('/', authorization, async (req, res) => {
     encontrado: req.body.encontrado
   }
 
-  if (newDesaparecido.is_animal && newDesaparecido.eventoId && ((newDesaparecido.pessoa_id && !newDesaparecido.animal_id) || (!newDesaparecido.pessoa_id && newDesaparecido.animal_id))) {
+  if (newDesaparecido.eventoId && ((newDesaparecido.pessoa_id && !newDesaparecido.animal_id) || (!newDesaparecido.pessoa_id && newDesaparecido.animal_id))) {
     try {
       const rows = await prisma.desaparecido.create({
         data: newDesaparecido
@@ -154,16 +154,15 @@ router.post('/', authorization, async (req, res) => {
   }
 });
 
-//todo
 router.delete('/', authorization, async (req, res) => {
   if (req.body.id) {
-    var eventoId = parseInt(req.body.id);
+    var desaparecidoId = parseInt(req.body.id);
     try {
-      const rows = await prisma.evento.delete({
+      const rows = await prisma.desaparecido.delete({
         where: {
-          id: eventoId
+          id: desaparecidoId
         }
-      })
+      });
       if (rows) {
         return res.json({
           status: "success",
@@ -186,53 +185,49 @@ router.delete('/', authorization, async (req, res) => {
   });
 });
 
-//todo
 router.patch('/', authorization, async (req, res) => {
   if (req.body.id) {
-    var eventoId = parseInt(req.body.id);
-    var newEvento = {
-      ativo: req.body.ativo,
-      nome: req.body.nome,
-      descricao: req.body.descricao,
-      data: req.body.data,
-      localidade_x: req.body.localidade_x,
-      localidade_y: req.body.localidade_y,
-      localidade_r: req.body.localidade_r,
-      endereco: req.body.endereco,
-      bairro: req.body.bairro,
-      cidade: req.body.cidade,
-      alterado_por: req.token.userId,
-      alterado_ip: req.ip,
-      alterado_em: new Date(Date.now())
+    var desaparecidoId = parseInt(req.body.id);
+    var newDesaparecido = {
+      is_animal: req.body.is_animal,
+      pessoa_id: req.body.pessoa_id,
+      animal_id: req.body.animal_id,
+      detalhes: req.body.detalhes,
+      eventoId: req.body.eventoId,
+      encontrado: req.body.encontrado
     }
 
-    try {
-      const rows = await prisma.evento.update({
-        where: {
-          id: eventoId
-        },
-        data: newEvento,
-      })
-      if (rows) {
-        return res.json({
-          status: "success",
-          message: "Recurso atualizado com sucesso",
-          animal: rows
+    if (newDesaparecido.eventoId && ((newDesaparecido.pessoa_id && !newDesaparecido.animal_id) || (!newDesaparecido.pessoa_id && newDesaparecido.animal_id))) {
+      try {
+        const rows = await prisma.desaparecido.update({
+          where: {
+            id: desaparecidoId
+          },
+          data: newDesaparecido,
         })
+        if (rows) {
+          return res.json({
+            status: "success",
+            message: "Recurso atualizado com sucesso",
+            animal: rows
+          })
+        }
       }
+      catch (err) {
+        console.log(err);
+      }
+      return res.status(500).json({
+        status: "error",
+        message: "Não foi possível atualizar esse recurso"
+      });
     }
-    catch (err) {
-      console.log(err);
-    }
-    return res.status(500).json({
+  }
+  else {
+    return res.status(400).json({
       status: "error",
-      message: "Não foi possível atualizar esse recurso"
+      message: "Parâmetros inválidos"
     });
   }
-  return res.status(400).json({
-    status: "error",
-    message: "Parâmetros inválidos"
-  });
 });
 
 module.exports = router;
